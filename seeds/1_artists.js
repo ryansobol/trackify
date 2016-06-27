28b4c9ab1c85138123cc7604edf1bd30c64b72df
Ryan Sobol <contact@ryansobol.com>
2016-06-27 15:41:30 -0700

    Initial commit

---
+++ seeds/1_artists.js
@@ -0,0 +1,14 @@
+'use strict';
+
+exports.seed = function(knex) {
+  return knex('artists').del()
+    .then(() => {
+      return knex('artists').insert([{
+        id: 1,
+        name: 'The Beatles'
+      }, {
+        id: 2,
+        name: 'Adele'
+      }]);
+    });
+};
