03e3a3ff755ee58736767bf66c53b1eeb5ac52e8
Ryan Sobol <contact@ryansobol.com>
2016-06-28 16:05:02 -0700

    Finish /artists routes

--- routes/artists.js
+++ routes/artists.js
@@ -4,11 +4,66 @@ const express = require('express');
 const router = express.Router();
 const knex = require('../knex');
 
-router.get('/artists', (req, res, next) => {
+router.get('/artists', (_req, res, next) => {
   knex('artists')
     .orderBy('id')
     .then((artists) => {
       res.send(artists);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.get('/artists/:id', (req, res, next) => {
+  knex('artists')
+    .where('id', req.params.id)
+    .first()
+    .then((artist) => {
+      res.send(artist);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.post('/artists', (req, res, next) => {
+  knex('artists')
+    .insert(req.body, '*')
+    .then((artists) => {
+      res.send(artists[0]);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.patch('/artists/:id', (req, res, next) => {
+  knex('artists')
+    .update(req.body, '*')
+    .where('id', req.params.id)
+    .then((artists) => {
+      res.send(artists[0]);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.delete('/artists/:id', (req, res, next) => {
+  knex('artists')
+    .where('id', req.params.id)
+    .first()
+    .then((artist) => {
+
+      return knex('artists')
+        .del()
+        .where('id', req.params.id)
+        .then(() => {
+          delete artist.id;
+          res.send(artist);
+        });
+
     })
     .catch((err) => {
       next(err);
