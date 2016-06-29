3ca1fccb4996ad16088b7672d6c938384d72b2ee
Ian Smith <iansmith1026@gmail.com>
2016-06-29 13:53:21 -0700

    Add user registration

--- routes/users.js
+++ routes/users.js
@@ -3,16 +3,26 @@
 const express = require('express');
 const router = express.Router();
 
+const knex = require('../knex');
 const bcrypt = require('bcrypt');
 
 router.post('/users', (req, res, next) => {
-  bcrypt.hash(req.body.password, 12, (err, hashed_password) => {
-    if (err) {
-      return next(err);
+  bcrypt.hash(req.body.password, 12, (hashErr, hashed_password) => {
+    if (hashErr) {
+      return next(hashErr);
     }
 
-    console.log(req.body.email, hashed_password);
-    res.sendStatus(200);
+    knex('users')
+      .insert({
+        email: req.body.email,
+        hashed_password: hashed_password
+      })
+      .then(() => {
+        res.sendStatus(200);
+      })
+      .catch((err) => {
+        next(err);
+      });
   });
 });
 
